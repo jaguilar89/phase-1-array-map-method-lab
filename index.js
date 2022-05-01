f78d2d1c57b9717604d1e9b80972abab09cfc068
Jose Aguilar <jaguilarny@gmail.com>
2022-05-01 14:59:42 -0400

    commit tree bb1992e12f4a9ca6107ad7486e71f19b346e7757

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+  const splitAndCasedArray = tutorials.map(title => title.split(' ')
+                                      .map(word => word[0].toUpperCase() + word.slice(1)))
+
+  const joinedArray = splitAndCasedArray.map(innerArray => innerArray.join(' '))
+  return joinedArray; 
 }
